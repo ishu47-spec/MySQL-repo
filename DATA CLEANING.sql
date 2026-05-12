e9a3cfe4d72cb7242bfc06a06ddc4a035901f8fd
ishu47-spec <ishankadal338@gmail.com>
2026-05-12 14:42:21 +0545

    Update DATA CLEANING.sql

--- DATA CLEANING.sql
+++ DATA CLEANING.sql
@@ -100,5 +100,52 @@ UPDATE layoffs_staging2
 SET country = 'United States'
 WHERE country LIKE 'United States%';
 
+-- Convert text date to proper date format
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+-- Change column type
+ALTER TABLE layoffs_staging2
+MODIFY column `date` DATE;
+
+-- REMOVING Null Values
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';         # now we fill the null by of Airbnb 1st by travel (of Airbnb 2nd)
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company             # First check if it's possible to fill
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND (t2.industry IS NOT NULL AND t2.industry != '');
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company             # First check if it's possible to fill
+    AND t1.location = t2.location
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND (t2.industry IS NOT NULL AND t2.industry != '');
+
+DELETE                      # deleting rows with null total_laid_off and percentage_laid_off
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
 
 
